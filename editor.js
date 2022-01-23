855ab2324fe7c252dc75164ab14dcd0e61bdc06c
Iulia Mieiță <mieitaiulia19@stud.ase.ro>
2022-01-23 19:12:28 +0200

    final adjusments

--- editor.js
+++ editor.js
@@ -1,19 +1,24 @@
 
 "use strict"
 
-//TODO: REFACTORING WITH ENCAPSULATION (too many global things)
+//TODO: MODULARIZARE (not proud about how I wrote my code)
 const c = document.getElementById("editable");
 const space = document.getElementsByClassName("editor-zone")[0];
 let myImage=null;
-let selectionMode=false; //daca sunt in mod selectie sau nu // strict pentru vizualizarea selectiei pe canvas
+let selectionMode=false; //variabila care controleaza selectia pe canvas pentru estetica (conturul selectiei real-time)
+const select = document.getElementById("view-mode");
+select.value="scrollable";
 
-///incarcarea imaginii in canva
+
+
+//switch dintre div-ul estetic de la lansarea aplicatiei si elementul canvas
 space.addEventListener('dragover', (e)=>{
 	space.style.display="none";
 	c.style.display="flex";
 	c.style.borderRadius="1px";
 })
 
+///incarcarea imaginii in canva
 document.addEventListener('dragover', function(e){e.preventDefault();})
 document.addEventListener("drop", function(e) {
     e.preventDefault();
@@ -26,29 +31,29 @@ document.addEventListener("drop", function(e) {
 			var height = img.naturalHeight;
 		
             const context = c.getContext('2d');
-			
+			//setez dimensiunile canvas-ului, din css acesta devine scrollable
 			c.width=width;
 			c.height=height;
 			
-            context.drawImage(img, 0, 0, width, height);
-			myImage=img;
+            context.drawImage(img, 0, 0, width, height); //desenare efectiva
+			myImage=img; //salvez imaginea pentru prelucrari
 
-			//set view mode value 
-			console.log("load;");
-			const select = document.getElementById("view-mode");
+			//setez modul de vizualizare la 100%, scrollable canvas pentru dimensiuni extrem de mari care nu ar fi putut fi incarcate
 			select.value="scrollable"; 
 			
-			//set selection to the whole canvas
+			//implicit toata imaginea este selectata o data incarcata 
+			//rectangle este un container pe care mi l-am definit pentru selectie
+			//si care este dependent de evenimentele de mouseup, mousedown, mousemove pe canva
 			rectangle.startX=0;
 			rectangle.startY=0;
 			rectangle.endX=myImage.width;
 			rectangle.endY=myImage.height;
-			rectangle.status="active";
-			rectangle.context=context;			
+			rectangle.status="active"; //adica selectia este activata, se pot aplica efecte, face crop..
+			rectangle.context=context; //not best practice, dar aveam nevoie pentru desenul dinamic al selectiei			
 		})
 		img.setAttribute('src', e.target.result);
 	})
-	reader.readAsDataURL(files[0]);
+	reader.readAsDataURL(files[0]); //imaginea incarcata
 
 })
 
@@ -73,7 +78,7 @@ save.onclick = function(){
 
 //my selection
 const rectangle = {
-	startX: null,
+	startX: null, 
 	startY: null,
 	endX:null,
 	endY:null,
@@ -81,13 +86,14 @@ const rectangle = {
 	status:null	
 }
 
-//select particularizat
+//select particularizat folosind evenimentele pe canvas pentru a detecta selectia utilizatorului
 c.onmousedown=getDimension;
 c.onmousemove=getDimension;
 c.onmouseup=getDimension;
 //intru in mod selectie.
 
 
+//am implementat stergerea (pixelii devin albi) la apasarea tastei d
 document.addEventListener("keydown", function(e){
 	if(rectangle.status==="active"&&e.key==="d"){
 		deleteSelection();
@@ -98,50 +104,59 @@ document.addEventListener("keydown", function(e){
 function deleteSelection() {
 	if(rectangle.context!=null){
 		rectangle.context.fillStyle="white";
+		//redesenez imaginea deoarece pentru cazul de selectie, mi-ar ramane conturul datorita implementarii mele
 		rectangle.context.drawImage(myImage, 0, 0, myImage.width, myImage.height);
+		//abia acum sterg pixelii din imaginea originala, fara selectia peste ea
 		rectangle.context.fillRect(rectangle.startX,rectangle.startY,rectangle.endX-rectangle.startX,rectangle.endY-rectangle.startY);
 		myImage.src=c.toDataURL();
-		rectangle.context=null; //selectia nu mai exista.
+		rectangle.context=null; //selectia se anuleaza
 	}
 }
 
+
 function getDimension(e){
 
+	//coordonatele intiale de la care s-a inceput selectia, activez selectia pana la event-ul de mouse-down
+	if(e.type==="mousedown"){
+		rectangle.context=null;
+		c.style.cursor="crosshair";
+		rectangle.startY=e.offsetY;
+		rectangle.startX=e.offsetX;
+		selectionMode=true;
+	}
+
+	//tratez si acest event pentru ca vreau sa desenez continuu forma selectiei (cat timp selectionMode)
+	if(e.type==="mousemove"&&selectionMode){
+		rectangle.endX=e.offsetX;
+		rectangle.endY=e.offsetY;
+		drawSelection(e);
+	}
+	//selectie finalizata prin preluarea coordonatelor finale ce urmeaza a fi prelucrare
 	if(e.type==="mouseup"){
 		selectionMode=false;
 		rectangle.endX=e.offsetX;
 		rectangle.endY=e.offsetY;
-		drawSelection(e);	
-	}
-	if(e.type==="mousedown"){
-			rectangle.context=null;
-			c.style.cursor="crosshair"
-			rectangle.startY=e.offsetY;
-			rectangle.startX=e.offsetX;
-			selectionMode=true;
-	}
-	//tratez si acest event pentru ca vreau sa desenez continuu forma selectiei
-	if(e.type==="mousemove"&&selectionMode){
-			rectangle.endX=e.offsetX;
-			rectangle.endY=e.offsetY;
-			drawSelection(e);
-	}
+		drawSelection(e);
+	}	
 }
 
-function drawSelection(e){
+//desenarea selectiei peste canvas pentru vizualizare
+function drawSelection(e) {
+	//pentru event-ul de mousemove si mousemove
 	if(rectangle.context!=null) {	
 		if(selectionMode){
-			rectangle.context.strokeStyle="green";
+			rectangle.context.strokeStyle="green"; //utilizatorul nu a terminat de selectat (nu s-a facut mouseup)
 			
 		} else {
-			rectangle.context.strokeStyle="white";
-			rectangle.status="active"; ////se poate sterge daca exista un context valid (press d)	
+			rectangle.context.strokeStyle="white"; //s-a terminat selectia
+			rectangle.status="active"; //se poate sterge daca exista un context valid (press d), face crop	
 		}
+		//in fiecare caz, redesenez imaginea initiala pana la mouseup, care marcheaza finalul
 		rectangle.context.drawImage(myImage, 0, 0, myImage.naturalWidth, myImage.naturalHeight, 0,0, c.width, c.height);
-		//rectangle.context.save(); //TODO: POSIBLE ERROR
 		rectangle.context.strokeRect(rectangle.startX,rectangle.startY,rectangle.endX-rectangle.startX,rectangle.endY-rectangle.startY);
-	
-	} else { //i am getting the same context, this part works only for mouse down (primul mouse move)
+	}
+	//pentru mousedown (cand se incepe selectia), setez particularitatile stilului selectiei doar prima data avand acelasi context
+	else {
 		rectangle.context=c.getContext('2d');
 		rectangle.context.lineWidth="1";
 		rectangle.context.setLineDash([5,5]);
@@ -150,19 +165,24 @@ function drawSelection(e){
 	}
 }
 
+
+
 //icon crop = selectie activa - crop apasat - change canva
 const crop = document.getElementById("crop");
 crop.addEventListener("click", () => {
-	if(myImage!=null) {
+	//verific ca am o imagine incarcata in aplicatie
+	if(myImage!=null) { 
+		//verific ca am selectata o zona
 		if(rectangle.context==null){
 			//nu s-a facut nicio selectia sau a fost stearsa selectia 
 			alert("Select a zone before cropping!") 
 		} else {
-			//proportii pentru crop (in mod 100% nu au effect, dar la fit image ajusteaza selectia)
+			//proportii pentru crop (in mod 100% nu au effect, dar la fit-image ajusteaza selectia)
 			let ratioH = myImage.naturalHeight/c.height; 
 			let ratioW = myImage.naturalWidth/c.width;
 
 			//dimensiunea este data de diferetele start-end in modul
+			//modul pentru ca depinde de felul in care a inceput selectia (nu obligatoriul de la coordonate mici spre mari)
 			c.width=Math.abs(rectangle.endX-rectangle.startX);
 			c.height=Math.abs(rectangle.endY-rectangle.startY);
 
@@ -176,10 +196,14 @@ crop.addEventListener("click", () => {
 			c.width=Math.round(ratioW*c.width);
 			c.height=Math.round(ratioH*c.height);
 
+			//crop-ul efectiv (desenez doar partea respectiva din imagine)
 			rectangle.context.drawImage(myImage, startX,startY,c.width,c.height, 0,0, c.width, c.height);
 			
+			//setez noile dimnesiuni
 			myImage.height=c.height;
-			myImage.width=c.width;	
+			myImage.width=c.width;
+
+			//salvez stadiul imaginii	
 			myImage.src=c.toDataURL();
 		
 			//invalidez selectia ca sa nu se poata face crop din nou
@@ -196,24 +220,33 @@ crop.addEventListener("click", () => {
 const resize= document.getElementById("resize");
 resize.onclick=function(){
 	if(myImage!=null){
+		//am ales sa incerc ca fac totul putin mai dinamic si mi-am creat meniurile pentru anumite functionalitati
+		//si le am afisat doar la activarea acesteia
+
 		const f = document.getElementById("form-size");
 		f.style.display="block";
 		f.style.position="absolute";
-		dragElement(f);
+		dragElement(f); //optiunile din div/form pot si mutate oriunde isi doreste utilizatorul
 
+		//dimensiunile imaginii (input-types)
 		const h = document.getElementById("height");
 		const w = document.getElementById("width");
 
+		//setez dimensiunile curente ale imaginii, spre modificare
 		let height = c.height;
 		let width = c.width;
 		h.value=height;
 		w.value=width;
 
+		//ascund meniul de resize
 		document.getElementById('cancel').addEventListener('click', ()=> {
 			f.style.display="none";
 		})
+
+		//aplic modificarea
 		document.getElementById('btn-resize').addEventListener('click', (e)=> {
-			
+			//mic bug, nu am stiut cum sa tratez ce sa fac daca utilizatorul imi da valori foarte mici de redimensionare
+			//la scalare pierd foarte mult din calitate si la revenire la dimensiuni mari se pierd proportiile
 			if((h.value>=1) && (w.value>=1)) {
 				f.style.display="none";
 				myImage.height=h.value;
@@ -222,11 +255,12 @@ resize.onclick=function(){
 				c.width=myImage.width;
 				c.height=myImage.height;
 				context.drawImage(myImage, 0,0, c.width, c.height);
-				myImage.src=c.toDataURL();
+				myImage.src=c.toDataURL(); //save the image
 			}
 			else {
-				e.stopImmediatePropagation();
-				e.preventDefault();
+				//le resetez pentru a putea face redimensionarea corect
+				h.value=height;
+				w.value=width;
 				alert("Valoarea specificata trebuie sa fie mai mare ca 0.")
 			}
 		})
@@ -325,11 +359,12 @@ text.onclick= function(e) {
 	
 }
 
-
+//select element pentru mod de vizualizare imagine incarcata in aplicatie
 //TODO: BUG LA IMAGINI SUPER MICI INCARCATE, SE MARESTE IMAGINEA.
-// It's not a bug, it's a feature. :)
-const select = document.getElementById("view-mode");
+// I decided it's not a bug, it's a feature. :)
+//const select = document.getElementById("view-mode");
 select.onchange=changeView;
+
 function changeView() {
 	if(myImage!=null) {
 		if(select.value==="scrollable"){
@@ -362,6 +397,7 @@ function changeView() {
 //icon effect
 const effect=document.getElementById("addEffect");
 effect.addEventListener("click", function() {
+	//daca am o imagine in aplicatie, atunci pot afisa meniul de effecte
 	if(myImage!=null) {
 		const menu=document.getElementById("menu-effects");
 		if(menu.style.display==="none"){
@@ -370,11 +406,13 @@ effect.addEventListener("click", function() {
 			menu.style.display="none"
 		}
 
+
 		const grey = document.getElementById("gray");
 		const bright = document.getElementById("bright");
 		const dark = document.getElementById("dark");
 		const sharp = document.getElementById("sharpen");
-		const sobel = document.getElementById("sobel");
+		//convolutie
+		const sobel = document.getElementById("sobel"); //edge detection
 		const undo =document.getElementById("undo");
 
 		grey.onclick= greyEffect;
@@ -385,7 +423,6 @@ effect.addEventListener("click", function() {
 
 		undo.onclick = function() {
 			menu.style.display="none"
-			//rectangle.context.restore();
 		}
 	} else {
 		alert('Upload an image first.');
@@ -393,7 +430,6 @@ effect.addEventListener("click", function() {
 })
 
 //TODO: Refactoring. Foloseste asta si la crop. Cod duplicat.
-//De fapt mai destept ar fi sa faci tu corectia oricum in rectangle.
 function calculatePoints(){
 	let startX =  Math.min(rectangle.startX, rectangle.endX);
 	let startY = Math.min(rectangle.startY, rectangle.endY);
@@ -407,23 +443,26 @@ function calculatePoints(){
 function greyEffect() {
 	if(rectangle.context!=null){
 		let selection= calculatePoints();
+		//redesenez ca dispara selectia
 		rectangle.context.drawImage(myImage,0,0, myImage.width, myImage.height,0,0, c.width, c.height);
+		//extrag pixelii pentru prelucrare
 		const pixels = rectangle.context.getImageData(selection.startX,selection.startY, selection.widthX, selection.widthY);
 		for(let i=0; i<pixels.data.length; i+=4)
 		{
 			let r = pixels.data[i];
 			let g= pixels.data[i+1];
 			let b=  pixels.data[i+2];
-			let corectie = (r + g + b)/3;
-			pixels.data[i] = pixels.data[i+1] =pixels.data[i+2]= corectie;
+			let corectie = (r + g + b)/3; //formula pentru gray tones
+			pixels.data[i] = pixels.data[i+1] =pixels.data[i+2]= corectie; //aplic pentru tonuri
 		}
-		rectangle.context.data=pixels.data;
+		rectangle.context.data=pixels.data; //schimb canvas
 		rectangle.context.putImageData(pixels,selection.startX,selection.startY,0,0,selection.widthX, selection.widthY);
-		rectangle.context.save();
+		//rectangle.context.save();
 		myImage.src=c.toDataURL();
 	}
 }
 
+//ma asigur ca ma incadrez in parametrii
 function checkRGB(data) {
 	if(data>255){
 		data=255;
@@ -435,13 +474,14 @@ function checkRGB(data) {
 	return data;
 }
 
+
 function brightEffect(e) {
 	if(rectangle.context!=null){
 		let selection= calculatePoints();
 		rectangle.context.drawImage(myImage,0,0, myImage.width, myImage.height,0,0, c.width, c.height);
-		//TODO: CHECK IF WIDTHS DIFFERENT OF 0, if not crapa getImageData.
+		
 		const pixels = rectangle.context.getImageData(selection.startX,selection.startY, selection.widthX, selection.widthY);
-		let value=-10;
+		let value=-10; //aleasa de mine pentru diferenta
 		if(e.srcElement.id==="bright") {
 			value=10; }
 		for(let i=0; i<pixels.data.length; i+=4){
@@ -449,7 +489,8 @@ function brightEffect(e) {
 			pixels.data[i+1]+= value;
 			pixels.data[i+2]+= value;
 			
-			pixels.data[i]=checkRGB(pixels.data[i]);
+			//modific data image cu rezultatul corectat in urma adaugarii
+			pixels.data[i]=checkRGB(pixels.data[i]); 
 			pixels.data[i+1]=checkRGB(pixels.data[i+1]);
 			pixels.data[i+2]=checkRGB(pixels.data[i+2]);
 		}
@@ -458,6 +499,11 @@ function brightEffect(e) {
 		myImage.src=c.toDataURL();
 	}
 }
+
+
+
+//pentru filtrele de convolutie am folosit sursele recomandate in cadrul cursului 
+//https://www.html5rocks.com/en/tutorials/canvas/imagefilters/
 
 function sharpeningEffect(){
 	if(rectangle.context!=null){
@@ -494,11 +540,11 @@ function convolute(image, array, opaque){
 	const w = sw;
 	const h = sh;
 
-	var output = {
+	var dataResult = {
 		width: w, height:h, data: new Float32Array(w*h*4)
 	};
 
-	const pixels = output.data;
+	const pixels = dataResult.data;
 
 	for(let y=0;y<h;y++){
 		for(let x=0;x<w;x++){
@@ -530,22 +576,18 @@ function convolute(image, array, opaque){
 		}
 	}
 
-	return output;
+	return dataResult;
 }
 
 
 
-//convolution filter, we have to mix 
-//TODO: Selection shape gets in the way... after applying the effect on a zone, then on the whole image..
 function sobelEffect(){
 	rectangle.context.drawImage(myImage,0,0, myImage.width, myImage.height,0,0, c.width, c.height);
-	//greyEffect();
-	//const pixels = ctx.getImageData(0,0,c.width, c.height); //whole image
+	
 	let selection = calculatePoints();
 	const pixels = rectangle.context.getImageData(selection.startX,selection.startY, selection.widthX, selection.widthY);
 	let vertical = convolute(pixels, [-1, 0, 1, -2, 0, 2, -1, 0, 1], false);
 	let horizontal = convolute(pixels, [-1, -2, -1, 0, 0, 0, 1, 2, 1], false);
-	//create Imagedata ?
 
 	let image = rectangle.context.createImageData(vertical.width, vertical.height);
 	for(let i=0;i<image.data.length;i+=4){
@@ -564,43 +606,36 @@ function sobelEffect(){
 }
 
 
-//////////////////////////////stole - TODO: refactoring 
-function dragElement(elmnt) {
+///////////////////////////////////////////////////////////////
+//utilitara pentru mutarea meniurilor
+function dragElement(menu) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+  menu.onmousedown = startMove;
 
-  function dragMouseDown(e) {
+  function startMove(e) {
     e = e || window.event;
-    //e.preventDefault();
-    // get the mouse cursor position at startup:
+    //pozitia initiala din div unde s-a inceput
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+    document.onmouseup = dragEnd; //pozitie finala
+    document.onmousemove = moveMenu; //actualizare si mutare
   }
 
-  function elementDrag(e) {
+  function moveMenu(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
+    //recalculez
+    pos1 = pos3 - e.clientX; 
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    
+	//meniul este reasezat
+    menu.style.top = (menu.offsetTop - pos2) + "px";
+    menu.style.left = (menu.offsetLeft - pos1) + "px";
   }
 
-  function closeDragElement() {
-    // stop moving when mouse button is released:
+  function dragEnd() {
     document.onmouseup = null;
     document.onmousemove = null;
   }
